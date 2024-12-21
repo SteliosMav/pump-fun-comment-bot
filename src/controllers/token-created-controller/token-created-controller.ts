6871aa4117b57e219f391de7061e52b4c6646456
Stelios Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2024-12-21 16:11:39 +0200

    Removed axios error interceptor and improved token-created-controller's err handling

--- src/controllers/token-created-controller/token-created-controller.ts
+++ src/controllers/token-created-controller/token-created-controller.ts
@@ -1,4 +1,4 @@
-import { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { TokenCreationEvent } from "../../listener/pump-fun-portal-listener";
 import { ProxyRotator } from "../../proxy/ProxyRotator";
 import { PumpFunService } from "../../pump-fun/pump-fun.service";
@@ -10,7 +10,6 @@ import { CommentGenerator } from "../../comment-generator/comment-generator";
 
 export class TokenCreatedController implements BasicController {
   private proxy = this.proxyRotator.proxy; // rotate through proxies
-  private lastActionTime = 0; // user for throttle
   private commentsCounter = 0; // used for data analysis
 
   constructor(
@@ -45,20 +44,27 @@ export class TokenCreatedController implements BasicController {
       COMMENT_MODE.type === "specific-token" ? COMMENT_MODE.mint : event.mint;
 
     try {
-      await this.comment(mint);
+      await this.pumpFunService.comment(
+        this.commentGenerator.comment,
+        mint,
+        this.accGen.account,
+        this.proxy
+      );
       this.commentsCounter++;
-      console.log(chalk.green(`Comment on "${mint}" succeeded at!`));
-      console.log("Comments counter: ", this.commentsCounter);
+      console.log(
+        chalk.green(
+          `Comment on "${mint}" succeeded at! Counter: ${this.commentsCounter}`
+        )
+      );
     } catch (e) {
-      const err = e as AxiosError;
-      if (err.status) {
+      if (isAxiosError(e)) {
         console.log(
           chalk.red(
-            `Comment on "${mint}" failed: ${err.status}. Error message: ${err.message}`
+            `Error while commenting. Status ${e.status}: ${e.response?.statusText}`
           )
         );
       } else {
-        console.log("Unknown error: ", err.status, err.message, this.proxy);
+        console.error(chalk.red("Non-Axios error:"), e);
       }
     }
 
@@ -71,42 +77,7 @@ export class TokenCreatedController implements BasicController {
     // Validation logic
     if (!event.mint) return null;
 
-    // Ensure `mint` is a string
+    // Ensure "mint" is a string
     return { ...event, mint: event.mint };
-  }
-
-  private shouldThrottle() {
-    const seconds = 2;
-    const delay = 1000 * seconds;
-    const currentTime = Date.now(); // Get the current timestamp in milliseconds
-
-    if (currentTime - this.lastActionTime >= delay) {
-      this.lastActionTime = currentTime; // Update the last action time
-      return false; // Indicate that the action can proceed
-    }
-
-    return true; // Indicate that the action should be skipped
-  }
-
-  private async comment(mint: string): Promise<AxiosResponse> {
-    const proxyToken = this.accGen.account;
-    const commentTxt = this.commentGenerator.comment;
-
-    // // Upload image and get its URI
-    // let fileUri: string;
-    // try {
-    //   fileUri = await this.pumpFunService.uploadImageToIPFS(proxyToken);
-    // } catch (e) {
-    //   throw { status: 0 };
-    // }
-
-    // Post comment
-    return this.pumpFunService.comment(
-      commentTxt,
-      mint,
-      proxyToken,
-      this.proxy
-      // fileUri
-    );
   }
 }
