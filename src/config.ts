ea6e6a9f964d4af142c9fdf618c4aad5e2ad9a53
Stelios Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2024-12-22 13:05:27 +0200

    Concurrent accounts are now based on the number of times event is processed

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import { CommentMode } from "./types";
 
 // Commenting
-export const DELAYS = [0, 1000 * 15]; // If exists, it will overwrite the above two options. Delays between requests in milliseconds
+export const DELAYS = [0, 0, 0, 0]; // If exists, it will overwrite the above two options. Delays between requests in milliseconds
 export const COMMENT_MODE: CommentMode = { type: "new-tokens" }; // Comment on new tokens or a specific token
 export const SAFE_COMMENTS = false; // Use comments that won't be detected as spam
 
 // Account generator
 export const ACCOUNTS_AHEAD = { min: 100, max: 200 }; // Starting from 0. Maximum number of accounts to create
-export const CONCURRENT_ACCOUNT_CREATION = 30; // Number of accounts to create in parallel
+export const CONCURRENT_ACCOUNT_CREATION = 15 * DELAYS.length; // Number of accounts to create in parallel
 export const PROFILE_FIELDS_TO_UPDATE: ("image" | "bio" | "username")[] = [
   "username",
   "image",
